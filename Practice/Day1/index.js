44d1685125ec16aafbedb36812fa419209256530
AmeenaCpt <shaikashikamina@gmail.com>
2026-02-16 18:40:31 +0530

    Day 1 - intro variables and dataTypes

---
+++ Practice/Day1/index.js
@@ -0,0 +1,18 @@
+const product={
+    name:"Parker Jotter Standard Ct ball Pen",
+    Color:"Black",
+    rating:4,
+    price:270,
+    offer:5
+}
+console.log(product);
+ const profile={
+    name:"Amina",
+    posts:195,
+    followers:569,
+    following:4,
+    isFollow:true,
+    Entrepreneur:"Apna Colege | Ex-MicroSoft,DRDO"
+    
+ }
+ console.log(typeof profile["name"]);
